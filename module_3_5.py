5c286b8d52f95975ef051759bf342281daf42c19
OfSv <konkovocso@gmail.com>
2024-10-19 12:46:33 +0300

    Рекурсия

---
+++ module_3_5.py
@@ -0,0 +1,14 @@
+# Задача "Рекурсия"
+# Подразумевается, что на вход подаётся целое число, и оно не начинается с 0
+
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+result = get_multiplied_digits(40203)
+print(result)
+
